43e51db16676020d4b5e18b69806be29df4fb9ab
hrttn <hrettien@gmail.com>
2023-10-17 18:25:25 +0200

    add prettier

--- game.js
+++ game.js
@@ -1,141 +1,137 @@
-const alreadyDrawnNumbers = [ 45, 63, 55, 60, 19, 23, 98, 33, 75 ]
+const alreadyDrawnNumbers = [60, 49, 1, 68, 23, 22, 84, 65, 77]
 
 const playedNumbers = []
 initGame()
 
-let nextNumber = alreadyDrawnNumbers[playedNumbers.filter(nb=>!!nb).length]
-const nextRandomNumberPlace = document.getElementById('nextRandomNumber')
+let nextNumber = alreadyDrawnNumbers[playedNumbers.filter((nb) => !!nb).length]
+const nextRandomNumberPlace = document.getElementById("nextRandomNumber")
 nextRandomNumberPlace.innerText = nextNumber
 
-const roundsPlayed = document.getElementById('numbersPlace')
+const roundsPlayed = document.getElementById("numbersPlace")
 
+function checkAllOptions(randomNumber) {
+	let availableOptions = 0
 
+	let closestOver = 101
+	let closestUnder = 0
 
-function checkAllOptions(randomNumber){
-    let availableOptions = 0
+	for (let i = 0; i < playedNumbers.length; i++) {
+		if (playedNumbers[i]) {
+			if (playedNumbers[i] < randomNumber) {
+				closestUnder = playedNumbers[i]
+			} else {
+				closestOver = playedNumbers[i]
+				break
+			}
+		}
+	}
 
-    let closestOver = 101
-    let closestUnder = 0
+	const closestUnderIndex = playedNumbers.findIndex((a) => a === closestUnder)
+	const closestOverIndex = playedNumbers.findIndex((a) => a === closestOver)
 
-    for (let i = 0; i < playedNumbers.length; i++){
-        if(playedNumbers[i]){
-            if(playedNumbers[i] < randomNumber){
-            closestUnder = playedNumbers[i]
-            } else {
-            closestOver = playedNumbers[i]
-            break
-            }
-        } 
-    }
+	for (let i = 0; i < playedNumbers.length; i++) {
+		const button = document.getElementById(`button-${i}`)
+		if (button.innerText === "") {
+			//Nothing has been placed
+			if (closestUnderIndex === -1 && closestOverIndex === -1) {
+				availableOptions = availableOptions + 1
+				button.disabled = false
+				button.classList = "button button-to-rank button--available"
+				continue
+			}
 
-    const closestUnderIndex = playedNumbers.findIndex(a=>a===closestUnder)
-    const closestOverIndex = playedNumbers.findIndex(a=>a===closestOver)
-    
-    for (let i = 0; i < playedNumbers.length; i++){
-        const button = document.getElementById(`button-${i}`)
-        if(button.innerText === ""){
-            //Nothing has been placed
-            if(closestUnderIndex === -1 && closestOverIndex === -1 ){
-                availableOptions = availableOptions + 1
-                button.disabled = false
-                button.classList = "button button-to-rank button--available"
-                continue
-            }
+			//All numbers placed are more than the next Number
+			if (closestUnderIndex === -1) {
+				if (i < closestOverIndex) {
+					availableOptions = availableOptions + 1
+					button.disabled = false
+					button.classList = "button button-to-rank button--available"
+				} else {
+					button.disabled = true
+					button.classList =
+						"button button-to-rank button--unavailable"
+				}
+				continue
+			}
 
-            //All numbers placed are more than the next Number
-            if(closestUnderIndex === -1){
-                if(i < closestOverIndex){
-                    availableOptions = availableOptions + 1
-                    button.disabled = false
-                    button.classList = "button button-to-rank button--available"
-                } else {
-                    button.disabled = true
-                    button.classList = "button button-to-rank button--unavailable"
-                }
-                continue
-            }
+			//All numbers placed are less than the next Number
+			if (closestOverIndex === -1) {
+				if (i > closestUnderIndex) {
+					availableOptions = availableOptions + 1
+					button.disabled = false
+					button.classList = "button button-to-rank button--available"
+				} else {
+					button.disabled = true
+					button.classList =
+						"button button-to-rank button--unavailable"
+				}
+				continue
+			}
 
-            //All numbers placed are less than the next Number
-            if(closestOverIndex === -1){
-                if(i > closestUnderIndex){
-                    availableOptions = availableOptions + 1
-                    button.disabled = false
-                    button.classList = "button button-to-rank button--available"
-                } else {
-                    button.disabled = true
-                    button.classList = "button button-to-rank button--unavailable"
-                }
-                continue
-            }
+			// Next number is between numbers placed
+			if (i > closestUnderIndex && i < closestOverIndex) {
+				availableOptions = availableOptions + 1
+				button.disabled = false
+				button.classList = "button button-to-rank button--available"
+			} else {
+				button.disabled = true
+				button.classList = "button button-to-rank button--unavailable"
+			}
+		}
+	}
 
-            // Next number is between numbers placed
-            if(i > closestUnderIndex && i < closestOverIndex){
-                availableOptions = availableOptions + 1
-                button.disabled = false
-                button.classList = "button button-to-rank button--available"
-            } else {
-                button.disabled = true
-                button.classList = "button button-to-rank button--unavailable"
-            }
-        }
-    }
-
-    return availableOptions > 0
+	return availableOptions > 0
 }
 
-function getRandomNumber(){
-    const canStillPlay = checkAllOptions(alreadyDrawnNumbers[playedNumbers.filter(nb=>!!nb).length])
-    if(canStillPlay){
-        return alreadyDrawnNumbers[playedNumbers.filter(nb=>!!nb).length]
-    } else {
-        document.body.style.backgroundColor = "#9A3B3B"
-        initGame()
-        return alreadyDrawnNumbers[playedNumbers.filter(nb=>!!nb).length]
-    }
-
+function getRandomNumber() {
+	const canStillPlay = checkAllOptions(
+		alreadyDrawnNumbers[playedNumbers.filter((nb) => !!nb).length]
+	)
+	if (canStillPlay) {
+		return alreadyDrawnNumbers[playedNumbers.filter((nb) => !!nb).length]
+	} else {
+		document.body.style.backgroundColor = "#9A3B3B"
+		initGame()
+		return alreadyDrawnNumbers[playedNumbers.filter((nb) => !!nb).length]
+	}
 }
 
-function initButtons(){
-    const buttonList = document.getElementById('buttonList')
-    buttonList.innerHTML = "";
-    document.body.style.backgroundColor = "#FFEEF4"
-    for (let counter = 0; counter < 9; counter = counter + 1){
-        let button = document.createElement("button")
-        button.className = "button button-to-rank button--available"
-        button.id = `button-${counter}`
-        button.dataset.index = counter
-        button.innerText = ""
-        button.addEventListener('click', () => {
-            if(button.innerText === ""){
-                button.innerText = nextNumber
-                button.disabled = true
-                button.className = "button button-to-rank button--filled"
-                playedNumbers.splice(button.dataset.index, 1 , nextNumber)
-                getNextRound()
-            } 
-        })
-        buttonList.appendChild(button)
-    }
+function initButtons() {
+	const buttonList = document.getElementById("buttonList")
+	buttonList.innerHTML = ""
+	document.body.style.backgroundColor = "#FFEEF4"
+	for (let counter = 0; counter < 9; counter = counter + 1) {
+		let button = document.createElement("button")
+		button.className = "button button-to-rank button--available"
+		button.id = `button-${counter}`
+		button.dataset.index = counter
+		button.innerText = ""
+		button.addEventListener("click", () => {
+			if (button.innerText === "") {
+				button.innerText = nextNumber
+				button.disabled = true
+				button.className = "button button-to-rank button--filled"
+				playedNumbers.splice(button.dataset.index, 1, nextNumber)
+				getNextRound()
+			}
+		})
+		buttonList.appendChild(button)
+	}
 }
 
-function initTurns(){
-    for (let counter = 0; counter < 9; counter = counter + 1){
-        playedNumbers[counter] = undefined
-    }
+function initTurns() {
+	for (let counter = 0; counter < 9; counter = counter + 1) {
+		playedNumbers[counter] = undefined
+	}
 }
 
-function initGame(){
-    initButtons()
-    initTurns()
+function initGame() {
+	initButtons()
+	initTurns()
 }
-
 
 function getNextRound() {
-    nextNumber = getRandomNumber()
-
-    nextRandomNumberPlace.innerText = nextNumber
-    roundsPlayed.innerText = alreadyDrawnNumbers.filter(nb=>!!nb).length - 1
-
-
+	nextNumber = getRandomNumber()
+	nextRandomNumberPlace.innerText = nextNumber
+	roundsPlayed.innerText = alreadyDrawnNumbers.filter((nb) => !!nb).length - 1
 }
-
